c577f45c35bd3f924ad7ae308ba60f6d3f12ad33
takch02 <lim70kr38@gmail.com>
2026-03-14 15:04:00 +0900

    feat : 문제 : 비동기 처리 TaskExecutor 에 한번에 많은 요청이 오며 DB 쓰레드 풀을 전부 이용하는 문제 발생. 해결 : 최대 30개의 요청까지 처리하고 나머지는 대기열에 넣는 방식으로 DB 쓰레드 풀이 마르지 않게 수정.

---
+++ src/main/java/com/highlight/highlight_backend/common/config/AsyncConfig.java
@@ -0,0 +1,27 @@
+package com.highlight.highlight_backend.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "taskExecutor") // 기본 taskExecutor를 덮어씌웁니다.
+    public Executor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);    // 기본 일꾼 10명
+        executor.setMaxPoolSize(30);     // 바빠도 최대 30명까지만 고용
+        executor.setQueueCapacity(100);  // 일꾼이 꽉 차면 100개까지만 대기열에 세움
+        executor.setThreadNamePrefix("Async-Worker-"); // 이름도 예쁘게 변경
+        
+        // 큐(100개)마저 꽉 차면 어떻게 할 것인가? (버리지 않고, 이벤트를 발생시킨 톰캣 스레드가 직접 처리하게 만듦)
+        executor.setRejectedExecutionHandler(new java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy());
+        
+        executor.initialize();
+        return executor;
+    }
+}
